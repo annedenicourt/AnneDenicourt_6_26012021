0cbaf218a9844092cfdef87c184e22fa1a4a3fe5
Anne Denicourt <annedenicourt@gmail.com>
2021-01-27 18:58:48 +0100

    helmet+mongoSanitize

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const path = require('path');
 
 mongoose.connect('mongodb+srv://annedenicourt:anitaoui@cluster0.l4vs4.mongodb.net/test?retryWrites=true&w=majority',
+//mongoose.connect(`mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@${process.env.CLUSTER_DB}.l4vs4.mongodb.net/${process.env.DATA_BASE_NAME}?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -22,6 +25,8 @@ app.use((req, res, next) => {
   });
 
 app.use(bodyParser.json());
+app.use(mongoSanitize());
+app.use(helmet());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', sauceRoutes);
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,19 +6,14 @@ const sauceCtrl = require('../controllers/sauce');
 
 //pour afficher toutes les sauces
 router.get('/', auth, sauceCtrl.getAllSauce);
-
 //pour enregistrer des sauces dans la BDD
 router.post('/', auth, multer, sauceCtrl.createSauce);
-
 //pour afficher une sauce
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-
 //pour modifier une sauce  
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-
 //pour liker une sauce  
 router.post('/:id/like', auth, sauceCtrl.likeSauce)
-
 //pour supprimer une sauce  
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
